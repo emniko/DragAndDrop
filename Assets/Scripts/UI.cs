d1209ff11ca79426b3e862eedd1b378b39c76866
Naqqash Khan <naqqash10@hotmail.com>
2020-08-08 21:19:19 +0500

    Initial Commit

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    public GameObject panel;
+    public GameObject controlBlocker;
+
+    public void OpenPanel() 
+    {
+        if (panel != null && controlBlocker != null) 
+        {
+            controlBlocker.SetActive(true);
+            panel.SetActive(true);
+        }
+    }
+
+    public void ClosePanel() 
+    {
+        if (panel != null && controlBlocker != null)
+        {
+            controlBlocker.SetActive(false);
+            panel.SetActive(false);
+        }
+    }
+
+
+}
